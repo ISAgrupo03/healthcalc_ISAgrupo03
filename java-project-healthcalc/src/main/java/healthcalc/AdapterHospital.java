bfd3c36b664305dd13a156a9218c144bbac284ef
palomamtnz13 <palomamtnz2004@gmail.com>
2026-05-31 01:22:56 +0200

    refactor: adaptar los patrones de diseño para las interfaces

--- java-project-healthcalc/src/main/java/healthcalc/AdapterHospital.java
+++ java-project-healthcalc/src/main/java/healthcalc/AdapterHospital.java
@@ -2,29 +2,33 @@ package healthcalc;
 
 public class AdapterHospital implements HealthHospital {
 	
-    private HealthCalc calc;
+    private ProxyHealthCalc calc;
 
-    public AdapterHospital(HealthCalc calc) {
+    public AdapterHospital(ProxyHealthCalc calc) {
         this.calc = calc;
     }
 
     @Override
     public int pesoCorporalIdeal(char genero, float altura) throws Exception {
     	
-        double alturaCm = altura * 100.0;
-        
-        double ibwKilos = calc.idealBodyWeight(alturaCm, genero);
-        
+        Gender genEnum= (Character.toUpperCase(genero)=='M') ? Gender.MALE : Gender.FEMALE;
+
+        Person patient= new Patient(70.0f, altura, genEnum, 30); 
+
+        float ibwKilos = calc.idealBodyWeight(patient);
+
         return (int) (ibwKilos * 1000);
     }
 
     @Override
     public Tuple<Float, String> indiceMasaCorporal(float altura, int peso) throws Exception {
     	
-        double pesoKilos = peso / 1000.0;
+        float pesoKilos = (float) (peso / 1000.0);
+
+        Person patient = new Patient(pesoKilos, altura, Gender.MALE, 30);
         
-        double valorBmi = calc.bmi(pesoKilos, altura);
-        String clasificacion = calc.bmiClassification(valorBmi);
+        float valorBmi = calc.bodyMassIndex(patient);
+        String clasificacion = calc.category(patient).toString();
         
         return new Tuple<>((float) valorBmi, clasificacion);
     }
